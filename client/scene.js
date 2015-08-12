2c898ad9a6c7a8603ad7fc3a80603215473aa097
John Rodney <jrodney@poeticsystems.com>
2015-08-11 20:50:02 -0500

    sphere movement, adds a util helper to copy vectors and maintain reference, adds rendered assets to public

--- client/scene.js
+++ client/scene.js
@@ -1,7 +1,10 @@
 Template.scene.rendered = function(){
 
   SceneManager.init();
-  SceneManager.addMultiple( [Utils.panoFactory( 'DantesView.jpg' , 'pano') , Utils.sphereFactory( 'target' )] );
+  SceneManager.addMultiple(
+    [ Utils.panoFactory( '40000.jpg' , 'pano' ),
+      Utils.sphereFactory( 'target', new THREE.Vector3(-50, 0, -4) )]);
+
   Utils.animate( SceneManager );
 
 };
--- utils/sphere-factory.js
+++ utils/sphere-factory.js
@@ -1,13 +1,21 @@
-Utils.sphereFactory = function(name) {
+Utils.sphereFactory = function(name, pos) {
+
   var geometry = new THREE.SphereGeometry( 2, 20, 20 ),
       material = new THREE.MeshBasicMaterial( {color: 0x00ffaa, wireframe: true} ),
       mesh = new THREE.Mesh( geometry, material );
 
   geometry.applyMatrix( new THREE.Matrix4().makeScale( -1, 1, 1 ) );
 
-  mesh.position.z = -10;
+  mesh.position = Utils.coordinateCopy(mesh.position, pos);
   mesh.name = name;
 
   return mesh;
 };
 
+/* Copy b to a */
+Utils.coordinateCopy = function(a, b) {
+  a.x = b.x;
+  a.y = b.y;
+  a.z = b.z;
+  return a;
+};
