ab92e965de028a7ae232109f5fcc29562d676c20
John Rodney <jrodney@poeticsystems.com>
2016-01-04 16:54:28 -0600

    seeds database with different images

--- server/seeds.js
+++ server/seeds.js
@@ -2,9 +2,9 @@ Meteor.startup(function() {
   if(Places.find().count() === 0){
     Places.insert({
       panos: [
-        panoFactory([waypointFactory(1, -25, 0, -10)], 'VJ2.jpg'),
-        panoFactory([waypointFactory(0, -35, 0, 0), waypointFactory(2, 35, 0, 0)], 'VJ1.jpg'),
-        panoFactory([waypointFactory(1, 3, 0, 35)], 'VJ3.jpg')
+        panoFactory([waypointFactory(2, -10, 0, -40), waypointFactory(1, 0, 0, 50)], 'lobby.jpeg'),
+        panoFactory([waypointFactory(0, 50, 0, -20)], 'poolday.jpeg'),
+        panoFactory([waypointFactory(0, -50, 0, 10)], 'courtyard.jpeg')
       ],
     });
   }
